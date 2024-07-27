3d682a8025a32e16529ff8cab63cc41eaa819c11
Yuya Hirayama <hiragram@gmail.com>
2024-07-27 15:07:46 +0900

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "exp-openapi-swift",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "exp-openapi-swift",
+            targets: ["exp-openapi-swift"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "exp-openapi-swift"),
+        .testTarget(
+            name: "exp-openapi-swiftTests",
+            dependencies: ["exp-openapi-swift"]),
+    ]
+)
---
+++ Tests/exp-openapi-swiftTests/exp_openapi_swiftTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import exp_openapi_swift
+
+final class exp_openapi_swiftTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
