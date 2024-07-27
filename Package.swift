9580181b29898c38a92eb53417fcfef39cfbb400
Yuya Hirayama <hiragram@gmail.com>
2024-07-27 21:56:04 +0900

    Restructure

--- Package.swift
+++ Package.swift
@@ -9,16 +9,16 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NantokaAPI",
-            targets: ["Generated"]),
+            targets: ["APIDefinition"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Generated"
+            name: "APIDefinition"
         ),
         .testTarget(
             name: "exp-openapi-swiftTests",
-            dependencies: ["Generated"]),
+            dependencies: ["APIDefinition"]),
     ]
 )
